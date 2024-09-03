03a08fc5e6708d8c3023a8b044fda5094fec0db7
Terrana Willma <terrana.w@gmail.com>
2024-09-03 20:22:15 +0700

    5menit

---
+++ index.js
@@ -0,0 +1,11 @@
+// input        // result
+//cari 3 :
+//[8 ,3 ,9 ,2 ,5] // 3
+
+//cari 1 :
+//[8 ,3 ,9 ,2 ,5] // false
+
+//cari "h" :
+//[8 ,3 ,9 ,2 ,5] // inputan harus number
+
+function linearSearch(arr, target) {}
